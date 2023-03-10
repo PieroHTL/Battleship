264d83f602eec4ed8560a6024f5c279c7e8b2c60
isusic96 <ivan9603@gmx.at>
2023-03-10 11:25:24 +0100

    Layout_Buttons
    10 x 10 Grid von Buttons erstellt ohne Funktion

--- Battleship_Main.py
+++ Battleship_Main.py
@@ -1,15 +1,33 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout
 
-# Create the application instance
-app = QApplication(sys.argv)
 
-# Create a window
-window = QWidget()
-window.setWindowTitle('Empty Main Window')
+class MyWidget(QWidget):
 
-# Show the window
-window.show()
+    def __init__(self):
+        super().__init__()
+        self.setMinimumSize(400, 300)
 
-# Start the event loop
-sys.exit(app.exec())
+        # Erstelle das Layout und füge die Knöpfe hinzu
+        layout = QGridLayout()
+        
+        # Erstelle die Knöpfe mit einer Schleife
+        self.buttons = []
+        for x in range(10):
+            for y in range(10):
+                button = QPushButton()
+                self.buttons.append(button)
+                layout.addWidget(button, x,y)
+        
+        
+        # Setze das Layout als Layout des Widgets
+        self.setLayout(layout)
+        #Fesnter benennen
+        self.setWindowTitle("Battleship")
+        
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    widget = MyWidget()
+    widget.show()
+    sys.exit(app.exec())
